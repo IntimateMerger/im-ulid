bf113d90e7d139e43c5345c4d4eed6a42c117059
higoshi <h33je5sg@gmail.com>
2021-07-15 22:12:29 +0900

    update and fixed

--- src/im-ulid.ts
+++ src/im-ulid.ts
@@ -1,7 +1,7 @@
 const charactors = '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
 const charactorLength = 32;
 
-export const imUlid = function() {
+export const imUlid = function () {
   let time = Date.now();
   let digit: number;
   let code = '';
@@ -14,15 +14,14 @@ export const imUlid = function() {
   }
 
   const randomList = new Uint8Array(15);
-
+  // eslint-disable-next-line
   // @ts-ignore
   ('crypto' in window ? crypto : msCrypto).getRandomValues(randomList);
 
   i = 16;
   while (i--) {
-    code += charactors.charAt(randomList[i] & charactorLength - 1)
+    code += charactors.charAt(randomList[i] & (charactorLength - 1));
   }
 
   return code;
 };
-
